1939e2796ec768005a46dd265631e4839972a9af
AzureZhen <rcraft@cyberconnects2.com>
2020-06-11 04:05:52 -0400

    Ballista added. Ammo still need done like SSG and SG

---
+++ src/main/java/mod/azure/doomweapon/item/weapons/Ballista.java
@@ -0,0 +1,40 @@
+package mod.azure.doomweapon.item.weapons;
+
+import mod.azure.doomweapon.DoomMod;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.world.World;
+
+public class Ballista extends CrossbowItem {
+
+	public Ballista(String name) {
+		super(new Item.Properties().group(DoomMod.DoomItemGroup));
+		this.setRegistryName(DoomMod.MODID, name);
+	}
+
+	@Override
+	public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
+		ItemStack stack = new ItemStack(this);
+		stack.hasTag();
+		stack.addEnchantment(Enchantments.PIERCING, 10);
+		if (group == DoomMod.DoomItemGroup) {
+			items.add(stack);
+		}
+	}
+
+	@Override
+	public void onCreated(ItemStack stack, World worldIn, PlayerEntity playerIn) {
+		stack.hasTag();
+		stack.addEnchantment(Enchantments.PIERCING, 10);
+	}
+
+	@Override
+	public boolean hasEffect(ItemStack stack) {
+		return false;
+	}
+}
