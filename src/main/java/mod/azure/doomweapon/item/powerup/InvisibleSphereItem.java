cdb1ba7202958844f1094a5db58f1652a0f0dca9
AzureZhen <rcraft@cyberconnects2.com>
2020-06-17 11:14:01 -0400

    Added Inmortal, Invisible, Mega, and Power Spheres and Soul Cube.

---
+++ src/main/java/mod/azure/doomweapon/item/powerup/InvisibleSphereItem.java
@@ -0,0 +1,12 @@
+package mod.azure.doomweapon.item.powerup;
+
+import mod.azure.doomweapon.DoomMod;
+import net.minecraft.item.Item;
+
+public class InvisibleSphereItem extends Item {
+
+	public InvisibleSphereItem() {
+		super(new Item.Properties().group(DoomMod.DoomItemGroup).maxStackSize(1));
+	}
+
+}
